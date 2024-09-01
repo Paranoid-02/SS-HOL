9ad00de9967e707c5b616f020b02d9530fe7f5b4
Akshay Sharma <akshay_02@outlook.com>
2024-09-01 22:17:24 +0530

    com

--- 18a.c
+++ 18a.c
@@ -8,52 +8,51 @@ Date: 28th Aug, 2024.
 ============================================================================
 */
 
-
-#include<fcntl.h>
-#include<unistd.h>
-#include<stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
 int main()
 {
-    struct {
-    int trainno;
-    int tickno;
-}db;
-    int fd=open("record.txt",O_RDWR);
+    struct
+    {
+        int trainno;
+        int tickno;
+    } db;
+    int fd = open("record.txt", O_RDWR);
     struct flock lock;
     int train;
     printf("Enter the train no (1,2,3,4)");
-    scanf("%d",&train);
-    lock.l_type=F_WRLCK;
-    lock.l_whence=SEEK_SET;
-    lock.l_start=(train-1)*sizeof(db);
-    lock.l_pid=getpid();
-    lseek(fd,(train-1)*sizeof(db),SEEK_SET);
+    scanf("%d", &train);
+    lock.l_type = F_WRLCK;
+    lock.l_whence = SEEK_SET;
+    lock.l_start = (train - 1) * sizeof(db);
+    lock.l_pid = getpid();
+    lock.l_len = sizeof(db);
+    lseek(fd, (train - 1) * sizeof(db), SEEK_SET);
     printf("PLease wait \n");
-    fcntl(fd,F_SETLKW,&lock);
-    read(fd,&db,sizeof(db));
-    printf("no of tickets sold is %d\n",db.tickno);
-    
+    fcntl(fd, F_SETLKW, &lock);
+    read(fd, &db, sizeof(db));
+    printf("no of tickets sold is %d\n", db.tickno);
+
     db.tickno++;
-    lseek(fd,-1*sizeof(db),SEEK_CUR);
-    
-    
-    lock.l_type=F_UNLCK;
+    lseek(fd, -1 * sizeof(db), SEEK_CUR);
+
+    lock.l_type = F_UNLCK;
     printf("press Enter to book ticket\n");
     getchar();
     getchar();
-    printf("Your ticket no is %d\n",db.tickno);
-    write(fd,&db,sizeof(db));
-    fcntl(fd,F_SETLKW,&lock);
-    
-    return 0;
+    printf("Your ticket no is %d\n", db.tickno);
+    write(fd, &db, sizeof(db));
+    fcntl(fd, F_SETLKW, &lock);
 
+    return 0;   
 }
 
 /*
 akshay~$ cc 18a.c
 akshay~$ ./a.out
 Enter the train no (1,2,3,4)3
-PLease wait 
+PLease wait
 no of tickets sold is 9
 press Enter to book ticket
 
